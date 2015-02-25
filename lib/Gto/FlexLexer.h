eb38f826cd96bf8cad0c973d0e8bd48616ded4b0
Jim Hourihan <jim@tweaksoftware.com>
2015-02-25 10:47:42 -0500

    Further sync of v4 from RV

--- lib/Gto/FlexLexer.h
+++ lib/Gto/FlexLexer.h
@@ -1,38 +1,6 @@
-//
-//  Copyright (c) 2009, Tweak Software
-//  All rights reserved.
-// 
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions
-//  are met:
-//
-//     * Redistributions of source code must retain the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer.
-//
-//     * Redistributions in binary form must reproduce the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer in the documentation and/or other materials
-//       provided with the distribution.
-//
-//     * Neither the name of the Tweak Software nor the names of its
-//       contributors may be used to endorse or promote products
-//       derived from this software without specific prior written
-//       permission.
-// 
-//  THIS SOFTWARE IS PROVIDED BY Tweak Software ''AS IS'' AND ANY EXPRESS
-//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-//  ARE DISCLAIMED. IN NO EVENT SHALL Tweak Software BE LIABLE FOR
-//  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-//  OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-//  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-//  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-//  DAMAGE.
-//
+// -*-C++-*-
+// FlexLexer.h -- define interfaces for lexical analyzer classes generated
+// by flex
 
 // Copyright (c) 1993 The Regents of the University of California.
 // All rights reserved.
@@ -85,6 +53,12 @@
 #    define FLEX_STD std::
 #  endif
 
+#if GTO_FLEX_APPLE == 1
+#define FLEX_SIZE_TYPE size_t
+#else
+#define FLEX_SIZE_TYPE int
+#endif
+
 extern "C++" {
 
 #include <algorithm>
@@ -94,6 +68,7 @@ extern "C++" {
 #include <string>
 #include <string.h>
 #include <vector>
+#include <stdio.h>
 #include <Gto/Reader.h>
 
 struct yy_buffer_state;
@@ -171,7 +146,7 @@ public:
 
     virtual int yylex();
     virtual void switch_streams( FLEX_STD istream* new_in, FLEX_STD ostream* new_out );
-#if GTO_FLEX_MINOR_VERSION >= 33
+#if GTO_FLEX_MINOR_VERSION > 33
     virtual int yywrap();
 #endif
 
@@ -186,8 +161,8 @@ public:
     int yyIdentifier(const char *);
 
 protected:
-    virtual int LexerInput( char* buf, int max_size );
-    virtual void LexerOutput( const char* buf, int size );
+    virtual FLEX_SIZE_TYPE LexerInput( char* buf, FLEX_SIZE_TYPE max_size );
+    virtual void LexerOutput( const char* buf, FLEX_SIZE_TYPE size );
     virtual void LexerError( const char* msg );
 
     void yyunput( int c, char* buf_ptr );
